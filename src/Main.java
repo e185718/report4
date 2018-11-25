fda702c95d80406bdc9022a37b8c9b1f9b20ac50
Daichi kise <e185718@ie.u-ryukyu.ac.jp>
2018-11-25 17:11:43 +0900

    step3 コードの準備

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        try {
+        String str = "百二十八";
+        int valuer = Integer.parseInt(str);
+        
+
+        /*try {
             String str = null;
             System.out.println(str.length());
-        }catch (NullPointerException e){
+         }catch (NullPointerException e){
             System.out.println("NullPointerExceptionが発生した");
             System.out.println(e.getMessage());
         }
+        */
+
+
     }
 }
+
